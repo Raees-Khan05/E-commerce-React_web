6d91abc187e8643188215de83db109b042b54522
Raees-Khan05 <154559811+Raees-Khan05@users.noreply.github.com>
2024-09-06 02:54:59 +0500

    Updated Code

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Route,  Routes } from 'react-router-dom'
-import Home from './components/Home'
+// import Home from './components/Home'
 import Products from './components/Products'
 import ProductDetail from './components/ProductDetail'
 import Header from './components/Header'
@@ -15,7 +15,7 @@ function App() {
     <BrowserRouter>
     <Header />
       <Routes>
-        <Route path='/'  element={<Home />} />
+        {/* <Route path='/'  element={<Home />} /> */}
         <Route path='/products'  element={<Products />} />
         <Route path='/products/:id' element={<ProductDetail />} />
       </Routes>
